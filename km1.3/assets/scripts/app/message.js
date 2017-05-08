e42c72449ef059750f4ab13d3056f54392362093
zhengjiawen <zhengjiawen@etouch.cn>
2017-05-08 18:58:45 +0800

    加入卡片列表页面，message由后台控制跳转

--- km1.3/assets/scripts/app/message.js
+++ km1.3/assets/scripts/app/message.js
@@ -6,11 +6,16 @@ define('app/message', function(require, exports, module) {
     });
     $('#conList').on('click', 'li', function(){
         var that = $(this);
-        if(that.data('type') == '3'){
-            window.location = 'kmb://main';
+        var action = that.data('action');
+        if(action.length > 6){
+            window.location = action;
         }else{
-            var f_id = $(this).data('fatherid');
-            window.location = 'kmb://comment?fatherid='+f_id;
+            if(that.data('type') == '3'){
+                window.location = 'kmb://main';
+            }else{
+                var f_id = $(this).data('fatherid');
+                window.location = 'kmb://comment?fatherid='+f_id;
+            }
         }
     })
 })
---
+++ km1.3/assets/scripts/app/myCardList.js
@@ -0,0 +1,26 @@
+define('app/myCardList', function(require, exports, module) {
+    var Ajax = require('../mod/base');
+    require('../plugs/swiper-2.7.min.js');
+    Ajax.custom({
+        url:'api/v1/cards/my'
+    }, function(d){
+        var endDate = d.data.endDate, startDate = d.data.startDate;
+        endDate !=0 && (d.data.endDate = endDate.replace(/-/g,'/'));
+        startDate !=0 && (d.data.startDate = startDate.replace(/-/g,'/'));
+        Ajax.render('#myCard', '#myCard-tmpl', d.data, undefined, true);
+    });
+    $('#activity_51').on('click', function(){
+        window.location = 'http://browser.kuaima.cn/51activity.html?auth_token='+Tools.auth_token();
+    })
+    Ajax.custom({
+        url:'api/v1/cards'
+    }, function(d){
+        Ajax.render('#cards', '#cards-tmpl', d, undefined, true);
+
+        new Swiper('#cardsWrap',{
+            freeMode : true,
+            loop : false,
+            slidesPerView : 'auto'
+        });
+    })
+});
