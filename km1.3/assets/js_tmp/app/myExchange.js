736ea394b0c31fd28a134613702f1c7d9a687aab
zhengjiawen <zhengjiawen@etouch.cn>
2017-08-07 16:37:05 +0800

    我的兑换

--- km1.3/assets/js_tmp/app/myExchange.js
+++ km1.3/assets/js_tmp/app/myExchange.js
@@ -2,6 +2,20 @@ define("app/myExchange", [ "../mod/pagelist", "../plugs/version" ], function(req
     var pagelist = require("../mod/pagelist");
     var km = require("../plugs/version");
     var lower = km.less("1.4.0");
+    pagelist.fun({
+        url: "api/v1/post/myExchange",
+        data: {
+            page: 1,
+            page_size: 20
+        }
+    }, function(d) {
+        var data = d.data;
+        for (var i = 0; i < data.length; i++) {
+            if (typeof data[i].post_images != "object" || data[i].post_images.length == 0) {
+                d.data[i].post_layout = 5;
+            }
+        }
+    });
 });define("mod/pagelist", [ "./base", "../plugs/laypage" ], function(require, exports, module) {
     var Ajax = require("./base");
     var laypage = require("../plugs/laypage");
--- km1.3/assets/scripts/app/myExchange.js
+++ km1.3/assets/scripts/app/myExchange.js
@@ -2,21 +2,21 @@ define('app/myExchange', function(require, exports, module) {
 	var pagelist = require('../mod/pagelist');
     var km = require('../plugs/version');
     var lower = km.less('1.4.0');
-    // pagelist.fun({ 
-    //     url: 'api/v1/readArticles',
-    //     data:{page: 1, page_size: 20}
-    // },null,function(){
-    //     //资源只有view1左右单图和view5无图
-    //     var w_ = $('.view_3 .imgbox').width() * .3;
-    //     $('.view_3 .imgbox').height(w_*74/113);
-    //     $('.view_3 .imgbox').each(function(){
-    //         var that = $(this);
-    //         if(that.find('img').length<1){
-    //             that.height(0)
-    //         }
-    //     })
+    pagelist.fun({ 
+        url: 'api/v1/post/myExchange',
+        data:{page: 1, page_size: 20}
+    },function(d){
+        var data = d.data;
+        for (var i = 0; i < data.length; i++) {
+            if(typeof data[i].post_images !="object" || data[i].post_images.length==0){
+                d.data[i].post_layout=5;
+            }
+        };
+    });
 
-    // });
+    // kmb://worthreadingreport?id={{=value.post_id}}
+
+    //kmb://worthreading?id=
 
     // $('#conList').on('click', 'li', function(){
     //     var url = $(this).data('url'),
