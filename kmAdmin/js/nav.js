0fc9a6747643c32880232eb4a36ac7655580ec07
zhengjiawen <zhengjiawen@etouch.cn>
2017-08-24 16:12:48 +0800

    使用iframe嵌套框架

---
+++ kmAdmin/js/nav.js
@@ -0,0 +1,335 @@
+var navs = [
+	{
+		"title": "Dashboard",
+		"icon": "&#xe62c;",
+		"spread": true,
+		"children": [{
+			"title": "汇总统计",
+			"href": "../dashboard/list.html"
+		}, {
+			"title": "任务情况",
+			"href": "../dashboard/read.html"
+		}, {
+			"title": "收入",
+			"href": "../dashboard/income.html"
+		}, {
+			"title": "花费",
+			"href": "../dashboard/cost.html"
+		}, {
+			"title": "用户",
+			"href": "../dashboard/user.html"
+		}]
+	}, 
+	{
+		"title": "应用管理",
+		"icon": "&#xe857;",
+		"spread": true,
+		"children": [{
+			"title": "应用版本",
+			"href": "main.html"
+		}, {
+			"title": "广告位",
+			"href": "main.html"
+		}, {
+			"title": "广告位开关",
+			"href": "main.html"
+		}, {
+			"title": "广告管理",
+			"href": "main.html"
+		}, {
+			"title": "弹窗管理",
+			"href": "main.html"
+		}, {
+			"title": "系统通知",
+			"href": "main.html"
+		}, {
+			"title": "搜索补量",
+			"href": "main.html"
+		}]
+	}, 
+	{
+		"title": "文章管理",
+		"icon": "&#xe705;",
+		"spread": true,
+		"children": [{
+			"title": "分类管理",
+			"href": "main.html"
+		}, {
+			"title": "文章管理",
+			"href": "main.html"
+		}, {
+			"title": "文章抓取",
+			"href": "main.html"
+		}, {
+			"title": "文章排行榜",
+			"href": "main.html"
+		}]
+	},
+	{
+		"title": "值得看",
+		"icon": "&#xe735;",
+		"spread": true,
+		"children": [{
+			"title": "采集用户",
+			"href": "main.html"
+		}, {
+			"title": "采集用户申请",
+			"href": "main.html"
+		}, {
+			"title": "标签列表",
+			"href": "main.html"
+		}, {
+			"title": "采集列表",
+			"href": "main.html"
+		}, {
+			"title": "昨日采集推荐",
+			"href": "main.html"
+		}, {
+			"title": "用户采集",
+			"href": "main.html"
+		}, {
+			"title": "采集排行榜",
+			"href": "main.html"
+		}, {
+			"title": "有效阅读记录",
+			"href": "main.html"
+		}, {
+			"title": "文章抓取",
+			"href": "main.html"
+		}, {
+			"title": "举报管理",
+			"href": "main.html"
+		}, {
+			"title": "举报记录",
+			"href": "main.html"
+		}]
+	},
+	{
+		"title": "评论管理",
+		"icon": "&#xe63a;",
+		"spread": true,
+		"children": [{
+			"title": "评论列表",
+			"href": "main.html"
+		}]
+	},
+	{
+		"title": "分享任务",
+		"icon": "&#xe641;",
+		"spread": true,
+		"children": [{
+			"title": "任务管理",
+			"href": "main.html"
+		}, {
+			"title": "任务订单",
+			"href": "main.html"
+		}, {
+			"title": "阅读记录",
+			"href": "main.html"
+		}]
+	},
+	{
+		"title": "运营活动",
+		"icon": "&#xe61d;",
+		"spread": true,
+		"children": [{
+			"title": "活动列表",
+			"href": "../optActivity/list.html"
+		}, {
+			"title": "活动奖励记录",
+			"href": "../optActivity/rewardLogs.html"
+		}]
+	},
+	{
+		"title": "红包管理",
+		"icon": "&#xe62e;",
+		"spread": true,
+		"children": [{
+			"title": "红包列表",
+			"href": "../redpack/list.html"
+		}, {
+			"title": "红包配置",
+			"href": "../redpack/setting.html"
+		}]
+	},
+	{
+		"title": "卡券管理",
+		"icon": "&#xe638;",
+		"spread": true,
+		"children": [{
+			"title": "虚拟卡券",
+			"href": "../cards/list.html"
+		}, {
+			"title": "用户卡券列表",
+			"href": "../cards/user.html"
+		},{
+			"title": "阅读加速状态",
+			"href": "../cards/readAccelerate.html"
+		}]
+	},
+	{
+		"title": "金币管理",
+		"icon": "&#xe659;",
+		"spread": true,
+		"children": [{
+			"title": "金币规则",
+			"href": "../coinRule/list.html"
+		}, {
+			"title": "每日汇率",
+			"href": "../coinExchange/list.html"
+		},{
+			"title": "金币活动管理",
+			"href": "../activity/show.html"
+		}]
+	},
+	{
+		"title": "首页推送管理",
+		"icon": "&#xe609;",
+		"spread": true,
+		"children": [{
+			"title": "推送规则",
+			"href": "../pushRule/list.html"
+		}, {
+			"title": "内容推荐队列",
+			"href": "../recommendQueue/list.html"
+		}]
+	},
+	{
+		"title": "用户召回",
+		"icon": "&#xe606;",
+		"href": "../recallPlan/list.html",
+		"spread": false
+	},
+	{
+		"title": "用户数据查询",
+		"icon": "&#xe612;",
+		"spread": true,
+		"children": [{
+			"title": "基本信息",
+			"href": "../users/base.html"
+		}, {
+			"title": "金币数据",
+			"href": "../users/coin.html"
+		}, {
+			"title": "现金流水",
+			"href": "../users/cash.html"
+		}, {
+			"title": "好友列表",
+			"href": "../inviteRelation/friends.html"
+		}, {
+			"title": "登录日志",
+			"href": "../users/loginLog.html"
+		}, {
+			"title": "搜索记录",
+			"href": "../users/searchLog.html"
+		}, {
+			"title": "资源兑换",
+			"href": "../post/exchangeOrders.html"
+		}, {
+			"title": "收徒/收益排行榜",
+			"href": "../top/list.html"
+		}, {
+			"title": "验证码",
+			"href": "../admin/verifyCode.html"
+		}]
+	},
+	{
+		"title": "数据统计",
+		"icon": "&#xe629;",
+		"spread": true,
+		"children": [{
+			"title": "签到",
+			"href": "../tj/checkin.html"
+		}, {
+			"title": "红包",
+			"href": "../tj/common.html?eventType=receiveRedPack"
+		}, {
+			"title": "邀请好友",
+			"href": "../tj/common.html?eventType=inviteFriend"
+		}, {
+			"title": "阅读文章",
+			"href": "../tj/common.html?eventType=readArticle"
+		}, {
+			"title": "采集",
+			"href": "../tj/post.html"
+		}, {
+			"title": "分享任务",
+			"href": "../tj/browserTask.html"
+		},{
+			"title": "评论",
+			"href": "../tj/comment.html"
+		},{
+			"title": "评论点赞",
+			"href": "../tj/common?eventType=commentIsLiked"
+		}, {
+			"title": "搜索",
+			"href": "../tj/searchLog.html"
+		}, {
+			"title": "金币汇总",
+			"href": "../tj/coin.html"
+		}]
+	},
+	{
+		"title": "财务数据",
+		"icon": "&#xe62d;",
+		"href": "../tj/date.html",
+		"spread": false
+	},
+	{
+		"title": "提现充值",
+		"icon": "&#xe65e;",
+		"spread": true,
+		"children": [{
+			"title": "提现",
+			"href": "main.html"
+		}, {
+			"title": "充值",
+			"href": "main.html"
+		}, {
+			"title": "兑吧订单列表",
+			"href": "main.html"
+		}, {
+			"title": "提现/充值记录",
+			"href": "main.html"
+		}, {
+			"title": "提现账号查询",
+			"href": "main.html"
+		}]
+	},
+	{
+		"title": "黑名单管理",
+		"icon": "&#xe69c;",
+		"spread": true,
+		"children": [{
+			"title": "黑名单",
+			"href": "main.html"
+		},{
+			"title": "白名单",
+			"href": "main.html"
+		}]
+	},
+	{
+		"title": "敏感词管理",
+		"icon": "&#xe6b2;",
+		"href": "../sensitiveWords/list.html",
+		"spread": false
+	},
+	{
+		"title": "系统配置/监控",
+		"icon": "&#xe631;",
+		"spread": true,
+		"children": [{
+			"title": "配置管理",
+			"href": "main.html"
+		}, {
+			"title": "后台操作日志",
+			"href": "main.html"
+		}, {
+			"title": "Redis监控",
+			"href": "main.html"
+		}, {
+			"title": "DB监控",
+			"href": "main.html"
+		}]
+	}
+];
