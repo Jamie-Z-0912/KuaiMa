fe62ae1383964b0d051bce6008f192b283b65027
zhengjiawen <zhengjiawen@etouch.cn>
2018-01-09 21:30:41 +0800

    运营：一元提现活动

---
+++ km1.5/assets/scripts/app/static_yiyuan.js
@@ -0,0 +1,32 @@
+define('app/yiyuan', function(require, exports, module) {
+	var Ajax = require('../mod/base');
+    var km =  require('../plugs/version.js');
+
+    document.getElementById('share').addEventListener("click", function(){
+        _hmt.push(['_trackEvent', 'yiyuan0111', 'click', 'share']);
+    })
+    document.getElementById('tixian').addEventListener("click", function(){
+        _hmt.push(['_trackEvent', 'yiyuan0111', 'click', 'tixian']);
+    });
+
+    if(km.isKM){
+        $('#share').parent().removeClass('hide');
+        $('#share').on('click',function(){
+            var mylink = location.href;
+            var share_kmb = 'kmb://share?param={"shareurl":"'+mylink+'","desc":"一元就能提现"}';
+            if (!km.less('1.5.5')) {
+                share_kmb = 'kmb://share?param={"shareurl":"'+mylink+'","desc":"低门槛，高效率，就是牛","title":"一元就能提现","icon":"http://static.etouch.cn/imgs/upload/1512982615.3361.png"}';
+            };
+            window.location = share_kmb;
+        });
+        $('#tixian').on('click',function(){
+            window.location = 'kmb://openduiba';
+        })
+    }else{
+        $('#share').parent().addClass('single').removeClass('hide');
+        $('#tixian').on('click',function(){
+            window.location = 'http://a.app.qq.com/o/simple.jsp?pkgname=com.kuaima.browser';
+        })
+    }
+
+});
