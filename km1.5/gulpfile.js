882b208dca48fa4675c1f62a191d680a71e659c1
zhengjiawen <zhengjiawen@etouch.cn>
2018-01-13 14:29:54 +0800

    删除打包中间生成的临时文件

--- km1.5/gulpfile.js
+++ km1.5/gulpfile.js
@@ -92,6 +92,7 @@ gulp.task('html', ['allLess', 'scripts_uglify'], function () {
         minifyJS: true,  //压缩页面JS
         minifyCSS: true  //压缩页面CSS
     };
+    gulp.src('./assets/js_tmp').pipe(clean());
     gulp.src(['./dist/rev/**/*.json', './assets/**/*.html'])
         .pipe(revCollector({
             replaceReved: true,
